06f583a4f67eddd840981e9526a95187f0eda964
Sarthak Aglawe <115921890+sarthak9001@users.noreply.github.com>
2026-02-28 20:56:11 -0800

    Update RPSGame.java

--- RPSGame.java
+++ RPSGame.java
@@ -1,11 +1,11 @@
 import java.util.Random;
 import java.util.Scanner;
-public class day20 {
+public class test {
     public static void main(String[] args) {
         Random rn = new Random();
         Scanner sc = new Scanner(System.in);
         
-int a = rn.nextInt(4);// to generate a random number upto 0-3
+int a = rn.nextInt(3)+1;// to generate a random number upto 0-3
 System.out.println("======== Rock,Paper,Seasor Game ========");
 System.out.println("Choose your Option = \n1 = Rock\n2 = Paper\n3 = Seasor");
 System.out.print("= ");
@@ -23,45 +23,39 @@ case 3:
 
     default:
         break;
+}    System.out.println();
+    System.out.println(" System choose =");
+
+switch (a) {
+    case 1:
+        System.out.print("Rock");
+        break;
+case 2:
+        System.out.print("Paper");
+        break;
+case 3:
+        System.out.println("Seasor");
+    default:
+        break;
 }
-System.out.println();
-System.out.print("System Choose = ");
-System.out.print(a);
-if(a==1){
-System.out.println(" = Rock");
+if (num == a) {
+    System.out.println();
+    System.out.println("=[ Game Tie ]=");
 }
-else if(a==2){
-    System.out.println(" = Paper");
-}
-else if(a==3){
-    System.out.println(" = Seasor");
-}
-System.out.print("Game = ");
-if (a==2){
-    System.out.println("Loss");
-}
-else if (a==3){
-    System.out.println("Win");
-}
-else if (a==1){
-    System.out.println("Win");
-}
-else if (a==3){
-    System.out.println("Loss");
-}
-else if (a==1){
-    System.out.println("Loss");
-}
-else if (a==2){
-    System.out.println("Win");
-}
-else if (a==2){
-    System.out.println("Loss");
-}
+else if((num==1 && a==2)||
+        (num==2 && a==3)||
+        (num==3 && a==1)){
+            System.out.println();
+            System.out.println("=[ You loos ]=");
+        }
 else{
-    System.out.println("tie");
+        System.out.println();
+
+    System.out.println(" Win");
 }
-System.out.println("");
+System.out.println("Thanks for playing the game");
+System.out.println("=============================================");
+
+System.out.println();
 }
-    
 }
